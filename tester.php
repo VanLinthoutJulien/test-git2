14848e465ea6bf556767c3be22b7dd26b7651ff7
JulienVL <jvanlint@gmail.com>
2018-11-13 16:37:15 +0100

    Initial commit
    (cherry picked from commit 6f68f7ec86882c560b6106aa8058be52ffca7b95)
    
    # Conflicts:
    #	README.md

---
+++ tester.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=noshrink-to-fit=no">
+        <title></title>
+        <meta name="descritption" content=""/>
+    </head>
+    <body>
+
+    </body>
+</html>
